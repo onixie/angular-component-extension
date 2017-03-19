b588dec480284c98029f82c719a75565218ec60d
monoid911 <onixie@gmail.com>
2017-03-19 18:38:35 +0900

    Remove unnecessary async on activate

--- src/extension.ts
+++ src/extension.ts
@@ -7,7 +7,7 @@ import * as vscodeCssLangServ from 'vscode-css-languageservice';
 
 // this method is called when your extension is activated
 // your extension is activated the very first time the command is executed
-export async function activate(context: vscode.ExtensionContext) {
+export function activate(context: vscode.ExtensionContext) {
     // Use the console to output diagnostic information (console.log) and errors (console.error)
     // This line of code will only be executed once when your extension is activated
     console.log('Congratulations, your extension "angular-component-extension" is now active!');
